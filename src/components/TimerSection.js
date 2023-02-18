f1897bbf55ab057f8d43d81755dad14b0caa5971
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-02-18 12:26:22 +0530

    feat: timer desing updated

--- src/components/TimerSection.js
+++ src/components/TimerSection.js
@@ -4,15 +4,18 @@ import Count from "./CountDown";
 const Timer = ()=>{
     return(
         <div>
-            <div className="card w-96 bg-base-100 shadow-stone-500 shadow-inner shad">
+            <div className="card w-auto bg-base-100 shadow-stone-500 shadow-inner">
+                <div className="px-3 tabs w-full">
+                    <a className="tab  tab-lg tab-lifted tab-active">Promodoro</a> 
+                    <a className="tab tab-lg tab-lifted">Short Break</a> 
+                    <a className="tab tab-lg tab-lifted">Long Break</a>
+                </div>
                 <figure className="px-10 pt-10">
                     <Count></Count>
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary text-4xl">START</button>
                     </div>
                 </div>
             </div>
