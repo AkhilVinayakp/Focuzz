2f8088e7cbede36044ba45d7047f3a488143b26d
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-02-18 15:27:02 +0530

    dev(action.types): added basis actions

--- src/context/action.types.js
+++ src/context/action.types.js
@@ -2,3 +2,9 @@
  * @description: Contains actions used inside reducers
  * and inside other components according to the requirements;
  ************************************/
+export const START_TIMER = "START_TIMER"
+export const STOP_TIMER = "STOP_TIMER"
+export const START_SB = "START_SMALL_BREAK";
+export const STOP_SB = "STOP_SMALL_BREAK";
+export const START_LB = "START_LONG_BREAK";
+export const STOP_LB = "STOP_LONG_BREAK";
