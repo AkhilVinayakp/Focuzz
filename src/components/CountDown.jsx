2e2bf4adb91be68c621b7f43722277933fe04aca
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-02-18 12:13:51 +0530

    fix: timer template style update

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -3,26 +3,14 @@ import React from "react";
 const Count = () =>{
     return(
         <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                    <span style={{"--value":15}}></span>
-                    </span>
-                    days
-                </div> 
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                    <span style={{"--value":10}}></span>
-                    </span>
-                    hours
-                </div> 
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
+                <div className="flex flex-col p-8 bg-neutral rounded-box text-neutral-content">
+                    <span className="countdown font-mono text-8xl">
                     <span style={{"--value":24}}></span>
                     </span>
                     min
                 </div> 
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
+                <div className="flex flex-col p-8 bg-neutral rounded-box text-neutral-content">
+                    <span className="countdown font-mono text-8xl">
                     <span style={{"--value":30}}></span>
                     </span>
                     sec
